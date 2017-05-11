5779d54478dd25c9c1aaaafa67f9d2d68b118450
Ilija Rankovic <ilijarankovic@gmail.com>
2017-05-11 20:47:22 +0200

    Napravljene klase FirstActivity i SecondActivity fixes #2

---
+++ app/src/main/java/com/example/korisnik/meni/SecondActivity.java
@@ -0,0 +1,77 @@
+package com.example.korisnik.meni;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by Korisnik on 11.5.2017..
+ */
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_second);
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onCreate", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onStart(){
+
+        super.onStart();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onStart", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onResume(){
+
+        super.onResume();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onResume", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onRestart(){
+
+        super.onRestart();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onRestart", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onPause(){
+
+        super.onPause();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onPause", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onStop(){
+
+        super.onStop();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onStop", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onDestroy(){
+
+        super.onDestroy();
+
+        Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onDestroy", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
